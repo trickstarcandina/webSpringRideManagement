4c76fe12f38f1ce493be3c0f72bd2c83579d0db7
TRICKSTAR\minht <rinnegan101220@gmail.com>
2021-12-04 15:25:42 +0700

    update Tuyen Xe

--- quanLyChuyenXe/src/main/java/com/example/quanlychuyenxe/controller/AdminController.java
+++ quanLyChuyenXe/src/main/java/com/example/quanlychuyenxe/controller/AdminController.java
@@ -1,13 +1,11 @@
 package com.example.quanlychuyenxe.controller;
 
+import com.example.quanlychuyenxe.base.response.ResponseBuilder;
 import com.example.quanlychuyenxe.model.TuyenXe;
 import com.example.quanlychuyenxe.services.TuyenXeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -16,8 +14,22 @@ public class AdminController {
 
     private TuyenXeService tuyenXeService;
 
+    //tuyen xe
     @PostMapping("/addTuyenXe")
     public ResponseEntity addTuyenXe(@RequestBody TuyenXe tuyenXe) {
         return ResponseEntity.ok(tuyenXeService.create(tuyenXe).build());
+    }
+
+    @DeleteMapping("/deleteTuyenXe/{idTuyenXe}")
+    public ResponseEntity deleteKhachHang(@PathVariable("idTuyenXe") String idTuyenXe) {
+        return ResponseEntity.ok().body(tuyenXeService.delete(idTuyenXe).build());
+    }
+
+    @PutMapping("/updateKhachHang/{idTuyenXe}")
+    public ResponseEntity updateKhachHang(@PathVariable("idTuyenXe") String idTuyenXe, @RequestBody TuyenXe tuyenXe) {
+        if(tuyenXe.getId().equals(idTuyenXe)) {
+            return ResponseEntity.ok().body(tuyenXeService.create(tuyenXe).build());
+        }
+        return ResponseEntity.ok().body(ResponseBuilder.ok(200, "Cập nhật thất bại"));
     }
 }
