7f7e37561f6d454e1f811b69bfd2c0041d2c7ffa
quocvu28032k <quocvu28032k@gmail.com>
2021-12-10 10:33:11 +0700

    client vu

--- quanLyChuyenXeClient/src/main/java/com/example/quanlychuyenxe/model/LuongCoBan.java
+++ quanLyChuyenXeClient/src/main/java/com/example/quanlychuyenxe/model/LuongCoBan.java
@@ -1,19 +1,34 @@
 package com.example.quanlychuyenxe.model;
 
 import com.example.quanlychuyenxe.base.entity.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Setter
 @Getter
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LuongCoBan extends BaseEntity {
+    @NotNull(message = "Thiếu lương")
     private Long luong;
+    @NotNull(message = "Thiếu phụ cấp")
     private Long phuCap;
+    @NotEmpty(message = "Thiếu ghi chú")
     private String ghiChu;
+    @NotNull(message = "Thiếu tháng lương")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", timezone = "Asia/Ho_Chi_Minh")
     private Date thangLuong;
 
     private String cmtTaiXe;
