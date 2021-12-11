6ab3cd1b5d9680326ec932f68b5071b81038d9cf
TRICKSTAR\minht <rinnegan101220@gmail.com>
2021-12-11 21:29:37 +0700

    update register

--- quanLyChuyenXeClient/src/main/java/com/example/quanlychuyenxe/controller/KhachHangController.java
+++ quanLyChuyenXeClient/src/main/java/com/example/quanlychuyenxe/controller/KhachHangController.java
@@ -14,10 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
 import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -104,5 +101,11 @@ public class KhachHangController {
         }
         model.addAttribute("khachhang", khachhang);
         return "khachhang/registerTrip";
+    }
+
+
+    @GetMapping("formregister")
+    private String formregister(Model model) {
+        return "khachhang/formregister";
     }
 }
