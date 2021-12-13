7ce09f187cfb52f30ab071748235bff5cd029b02
TRICKSTAR\minht <rinnegan101220@gmail.com>
2021-12-13 07:36:54 +0700

    add login admin

--- quanLyChuyenXe/src/main/java/com/example/quanlychuyenxe/controller/AdminController.java
+++ quanLyChuyenXe/src/main/java/com/example/quanlychuyenxe/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.example.quanlychuyenxe.controller;
 
 
+import com.example.quanlychuyenxe.dto.AuthenticationRequest;
 import com.example.quanlychuyenxe.model.*;
 import com.example.quanlychuyenxe.model.request.ChuyenXeRequest;
 import com.example.quanlychuyenxe.services.*;
@@ -16,6 +17,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/api/admin")
 @AllArgsConstructor
@@ -28,6 +31,7 @@ public class AdminController {
     private TaiXeService taiXeService;
     private ChuyenXeService chuyenXeService;
     private TongLuongService tongLuongService;
+    private AdminService adminService;
 
     private BCryptPasswordEncoder encoder;
 
@@ -209,5 +213,21 @@ public class AdminController {
     @GetMapping("/thongke/luongtaixe")
     public ResponseEntity thongkeTongLuong(@RequestParam("thang") Integer thang, @RequestParam("nam") Integer nam) {
         return ResponseEntity.ok().body(tongLuongService.getAllTongLuongByDate(thang, nam).build());
+    }
+
+    //auth
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody AuthenticationRequest authenticationRequest) {
+        return ResponseEntity.ok().body(adminService.findUser(authenticationRequest.getUsername(), authenticationRequest.getPassword()).build());
+    }
+
+    @PostMapping("/authen")
+    public ResponseEntity authen(@RequestBody AuthenticationRequest authenticationRequest, @RequestParam("code") String code) throws IOException {
+        return ResponseEntity.ok().body(adminService.authen(authenticationRequest.getUsername(), authenticationRequest.getPassword(), code).build());
+    }
+
+    @PostMapping("/showQRCode")
+    public ResponseEntity showQRCode(@RequestParam("displayName") String displayName) {
+        return ResponseEntity.ok().body(adminService.showQRCode(displayName).build());
     }
 }
---
+++ quanLyChuyenXe/src/main/java/com/example/quanlychuyenxe/services/AdminService.java
@@ -0,0 +1,11 @@
+package com.example.quanlychuyenxe.services;
+
+import com.example.quanlychuyenxe.base.response.Response;
+
+import java.io.IOException;
+
+public interface AdminService {
+    Response findUser(String username, String password);
+    Response authen(String username, String password, String code) throws IOException;
+    Response showQRCode(String displayName);
+}
